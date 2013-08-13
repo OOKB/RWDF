1049ac775e1424a8b74ef0e2cfedee16114dd298
Kristian Bjørnard <kristian.bjornard@gmail.com>
2013-08-13 17:32:02 -0400

    adding some images

--- js/rwdf.js
+++ js/rwdf.js
@@ -1,5 +1,7 @@
+// set common height of triptych blocks on homepage
 $(document).ready(function() {
   var blockwidth = $('.span4').width();
-  var blockheight = blockwidth/1.618;
+  var blockheight = blockwidth/1.32;
   $('.triptych .span4').height(blockheight);
 });
+
