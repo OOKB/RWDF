ffb21d94f5c8225ee88d80de26cc6eb92bbc7691
Kristian Bjørnard <kristian.bjornard@gmail.com>
2013-08-13 21:05:57 -0400

    updating the project pages

--- js/rwdf.js
+++ js/rwdf.js
@@ -5,6 +5,11 @@ $(document).ready(function() {
   $('.triptych .trio').height(blockheight);
 });
 
-
+// subtract height of projects so prev/next buttons show up in right places
+$(document).ready(function() {
+  var projectheight = $('article.project').height();
+  var relpos = (61+projectheight)*-1;
+  $('.minus-top .btn').css('top',relpos);
+});
 
 
